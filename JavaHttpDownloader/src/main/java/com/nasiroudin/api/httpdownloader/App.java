ca6d8dd129aa8a1409f61f06e091212aac17c525
Nasiroudin Peerbuccoss <nasiroudin18@gmail.com>
2015-09-10 21:13:56 +0400

    Added JavaHttpDownloader folder which is generated using maven

---
+++ JavaHttpDownloader/src/main/java/com/nasiroudin/api/httpdownloader/App.java
@@ -0,0 +1,13 @@
+package com.nasiroudin.api.httpdownloader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
